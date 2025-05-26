cad46a35b3b9b78a08bf8c863c1cc123e77af7ab
Alberto Carrasco Fernandez <acarrasco@epigram.es>
2025-05-26 16:25:05 +0200

    js

--- js.js
+++ js.js
@@ -5,6 +5,12 @@ window.addEventListener('scroll', () => {
         const elemento = document.getElementById('main');
         if (elemento) {
             elemento.scrollIntoView({ behavior: 'smooth' });
+
+            document.body.style.overflow = 'hidden';
+
+            setTimeout(() => {
+                document.body.style.overflow = 'auto';
+            }, 1500);
         }
         scrolledOnce = true;
     }
